11b08cc961cac55d105fbae61f3f5ab36c15104f
Miguel Angel Osonio <miguelos1128@gmail.com>
2022-10-29 16:54:58 -0500

    creacion de 2 archivos bigints y nullish

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,6 @@
+//anteriormente
+const aBigNumber = 123123456799876543n;
+//Forma actual
+const anotherBigNumber = BigInt(123123456799876543);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
